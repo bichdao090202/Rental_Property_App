078f851706ed8f69d2678963321a4bd91aa9059e
bichdao090202 <bichdao090202@gmail.com>
2024-09-20 08:38:05 +0700

    create contract from device

--- lib/widgets/home/file_picker.dart
+++ lib/widgets/home/file_picker.dart
@@ -1,4 +1,6 @@
 import 'dart:io' as io;
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -13,10 +15,11 @@ class SingleFilepickerScreen extends StatefulWidget {
 }
 
 class _SingleFilepickerScreenState extends State<SingleFilepickerScreen> {
-  // Variable to hold file name
   String? _fileName;
   Uint8List? _fileBytes; // Use for web to hold file content bytes
-  io.File? _file; // Use for mobile to hold File object
+  io.File? _file;
+  String? _base64File;
+
 
   Future<void> getFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles();
@@ -29,10 +32,18 @@ class _SingleFilepickerScreenState extends State<SingleFilepickerScreen> {
       } else {
         // If running on mobile (iOS/Android), use path
         final file = io.File(result.files.single.path!);
+        setState(() {
+          _fileName = file.path.split('/').last; // Lấy tên file
+        });
         _file = file;
         _fileName = file.path.split('/').last;
+        Uint8List fileBytes = await _file!.readAsBytes();
+        _base64File = base64.encode(fileBytes);
+        // _base64File = base64Encode(fileBytes);
       }
-      setState(() {});
+      setState(() {
+        print('Base64 của file: $_base64File');
+      });
     } else {
       // User canceled the picker
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
@@ -69,50 +80,55 @@ class _SingleFilepickerScreenState extends State<SingleFilepickerScreen> {
 
   @override
   Widget build(BuildContext context) {
+    print('Base64 của file: $_base64File');
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                _fileName != null ? "File Name: " : "No file selected",
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-
-          if (_fileName != null)
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
-                  _fileName!,
+                  _fileName != null ? "File Name: " : "No file selected",
                   textAlign: TextAlign.center,
                   style: const TextStyle(
                     color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ],
             ),
 
-          // Nút để mở PDF trong dialog
-          if (_file != null && _file!.path.endsWith('.pdf'))
-            ElevatedButton(
-              onPressed: () {
-                _showPdfDialog(_file!);
-              },
-              child: const Text("View PDF"),
-            ),
-        ],
+            if (_fileName != null)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    _fileName!,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+
+            // Nút để mở PDF trong dialog
+            if (_file != null && _file!.path.endsWith('.pdf'))
+              ElevatedButton(
+                onPressed: () {
+                  _showPdfDialog(_file!);
+                },
+                child: const Text("View PDF"),
+              ),
+            // if (_base64File != null)
+            //   Text("File Base64: $_base64File"),
+          ],
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
